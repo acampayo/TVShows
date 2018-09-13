ee1e2c915fe0c5251bea3fb8e13af13438a2afd9
Alex Campayo <alex.campayo@gmail.com>
2018-09-13 22:03:07 +0200

    Create base project

---
+++ app/src/main/java/com/tvshows/tvshows/TVShowsActivity.kt
@@ -0,0 +1,12 @@
+package com.tvshows.tvshows
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TVShowsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tvshows)
+    }
+}
