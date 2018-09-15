c96b4e14968cfbf1eb79029a7093e68a0663eea4
Alex Campayo <alex.campayo@gmail.com>
2018-09-15 15:56:37 +0200

    Define TV shows API

---
+++ app/src/main/java/com/tvshows/features/tvshows/TVShowsApi.kt
@@ -0,0 +1,24 @@
+package com.tvshows.features.tvshows
+
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+internal interface TVShowsApi {
+    companion object {
+        const val BASE_URL = "https://api.themoviedb.org/3"
+        private const val API_KEY = "cb87aff7d1a8dec7fe7ad2e7c183358f"
+        private const val API_KEY_PARAM = "api_key"
+        private const val PAGE_PARAM = "page"
+        private const val TV_SHOW_ID_PARAM = "tv_id"
+        private const val POPULAR_TV_SHOWS = "/tv/popular"
+        private const val SIMILAR_TV_SHOWS = "/tv/{$TV_SHOW_ID_PARAM}/similar"
+    }
+
+    @GET(POPULAR_TV_SHOWS)
+    fun popularTvShows(@Query(API_KEY_PARAM) apiKey: String, page: Int)
+
+    @GET(SIMILAR_TV_SHOWS)
+    fun similarTvShows(@Path(TV_SHOW_ID_PARAM) tvShowId: Int,
+                       @Query(API_KEY_PARAM) apiKey: String, page: Int)
+}
