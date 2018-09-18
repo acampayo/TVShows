2c21bbdcfe34e3591b88a357b605a2e0a9ef7c5a
Alex Campayo <alex.campayo@gmail.com>
2018-09-18 13:26:11 +0200

    Create get similar tv shows use case

--- app/src/main/java/com/tvshows/features/tvshows/GetSimilarTVShows.kt
+++ app/src/main/java/com/tvshows/features/tvshows/GetSimilarTVShows.kt
@@ -1,5 +1,14 @@
 package com.tvshows.features.tvshows
 
-class GetSimilarTVShows {
+import com.tvshows.core.exception.Failure
+import com.tvshows.core.functional.Either
+import javax.inject.Inject
 
+class GetSimilarTVShows
+@Inject constructor(private val repository: TVShowsRepository) {
+
+    operator fun invoke(page: Int, tvShowId: Int, onResult: (Either<Failure,
+            List<TVShow>>) -> Unit = {}) {
+        repository.similarTvShows(page, tvShowId, onResult)
+    }
 }
