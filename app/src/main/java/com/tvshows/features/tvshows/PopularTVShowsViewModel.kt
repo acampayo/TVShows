e92e026a7207e1057f3ee24d58e8f347fb493855
Alex Campayo <alex.campayo@gmail.com>
2018-09-18 11:41:33 +0200

    Refactor popular tv shows view model

--- app/src/main/java/com/tvshows/features/tvshows/PopularTVShowsViewModel.kt
+++ app/src/main/java/com/tvshows/features/tvshows/PopularTVShowsViewModel.kt
@@ -1,6 +1,7 @@
 package com.tvshows.features.tvshows
 
 import android.arch.lifecycle.MutableLiveData
+import com.tvshows.core.extension.addValues
 import com.tvshows.core.platform.BaseViewModel
 import javax.inject.Inject
 
@@ -8,21 +9,21 @@ class PopularTVShowsViewModel
 
 @Inject constructor(private val getPopularTVShows: GetPopularTVShows): BaseViewModel() {
 
-    var page = 0
+    var page = 1
     var tvShows: MutableLiveData<MutableList<TVShow>> = MutableLiveData()
 
     fun loadPopularTvShows() {
-        page++
         getPopularTVShows(page) {
             it.either(::handleFailure, ::handleTVShows)
         }
     }
 
     private fun handleTVShows(tvShows: List<TVShow>) {
-        if (this.tvShows.value == null) {
-            this.tvShows.value = tvShows.toMutableList()
-        } else {
-            this.tvShows.value?.addAll(tvShows)
+        when(this.tvShows.value == null) {
+            true -> this.tvShows.value = tvShows.toMutableList()
+            false -> this.tvShows.addValues(tvShows)
         }
+
+        page++
     }
 }
